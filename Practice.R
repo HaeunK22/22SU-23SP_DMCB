00e2c201f4ebc091464a0efd29a9641e2e1b2ed8
HaeunK22 <69032913+HaeunK22@users.noreply.github.com>
2022-07-22 16:09:03 +0900

    Fin Data Loading

--- Practice.R
+++ Practice.R
@@ -1,3 +1,7 @@
+# Execue : Ctrl + Enter
+
+#### Data Loading ####
+
 # Import library
 library(data.table)
 
@@ -8,11 +12,29 @@ setwd("C:/TCGA-LIHC/")
 clinical <- fread("TCGA.LIHC.sampleMap_LIHC_clinicalMatrix")  # "" 사이에서 tab 누르면 dir 안의 파일 확인
 expr <- fread("gene_expression", data.table = F)
 
-# expr에 있는 사람들이 모두 clinical에 있는지 확인
+# Expr에 있는 사람들이 모두 clinical에 있는지 확인
 all(colnames(expr) %in% clinical$sampleID)  # Output : TRUE
 
-# expr에서 gene_list 분리
+# Expr에서 gene_list 분리
 gene_list <- expr[,1]
 expr <- expr[,-1]
 
-# Execue : Ctrl + Enter
+# Clinical data의 사람 순서를 expression data의 순서에 맞추기
+idx <- NULL
+for (sample in colnames(expr)){
+  tmp = which(clinical$sampleID == sample)
+  idx <- c(idx, tmp)
+}
+length(idx)   # Expression data의 개수와 같은 지 확인
+clinical_core  <- clinical[idx,]
+
+colnames(expr)[1:5]   # 잘 맞췄는 지 확인
+clinical_core$sampleID[1:5]
+
+# Tumor와 Normal 분리
+tumor_idx <- which(clinical_core$sample_type == "Primary Tumor")
+expr_tumor <- expr[,tumor_idx]
+
+normal_idx <- which(clinical_core$sample_type == "Solid Tissue Normal")
+expr_normal <- expr[,normal_idx]
+
