b1f9ff598a2173bdf37d1baec17e665879e5e29a
HaeunK22 <69032913+HaeunK22@users.noreply.github.com>
2023-02-15 11:43:54 +0900

    Create dataload.py

---
+++ MLSurv/src/dataload.py
@@ -0,0 +1,20 @@
+"""Data loader"""
+import torch
+from torch.utils.data import Dataset
+import warnings
+warnings.filterwarnings('ignore')
+
+class Data(Dataset):
+    def __init__(self, X, time, observed, device):
+        self.x_data = torch.tensor(X, device=device)
+        self.time_data = torch.tensor(time, device=device)
+        self.observed_data = torch.tensor(observed, device=device)
+    
+    def __len__(self):
+        return len(self.x_data)
+    
+    def __getitem__(self, index):
+        x = torch.tensor(self.x_data[index]).clone().detach()
+        time = torch.tensor(self.time_data[index]).clone().detach()
+        observed = torch.tensor(self.observed_data[index]).clone().detach()
+        return x, time, observed
