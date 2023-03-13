9149df42991bc5b63f5671f205a90517a748a45f
HaeunK22 <69032913+HaeunK22@users.noreply.github.com>
2023-03-13 16:40:42 +0900

    MLDRL loss ver.

--- MLSurv/src/train.py
+++ MLSurv/src/train.py
@@ -1,14 +1,72 @@
+"""training with RNA and Methylation."""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import KFold
 from MLSurv import *
-from loss import *
-from dataload import *
+from loss import SurvLoss, latent_loss
+from dataload import Data
+from earlystopping import EarlyStopping
 from torch import nn
+import argparse
+from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader, TensorDataset
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.model_selection import KFold, cross_val_score
 from lifelines.utils import concordance_index
+from pycox.evaluation import EvalSurv
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--input1', '-i1', type=str, default='./../data/RNA_common_sorted.tsv')
+parser.add_argument('--input2', '-i2', type=str, default='./../data/methyl_common_sort.tsv')
+parser.add_argument('--label', '-l', type=str, default='./../data/labels_common.tsv')
+parser.add_argument('--feat_num', '-f', type=int, default=1000)
+
+config = parser.parse_args()
 
 cuda = True if torch.cuda.is_available() else False
 if cuda:
     device = torch.device(f'cuda:{0}' if torch.cuda.is_available() else 'cpu')
 
-input_dim = 512
+input_dim = config.feat_num
+
+stdscaler = StandardScaler()
+
+X1 = pd.read_table(config.input1, nrows = input_dim, header=None).transpose()
+X2 = pd.read_table(config.input2, nrows = config.feat_num).transpose()
+X1 = stdscaler.fit_transform(np.array(X1))
+X2 = stdscaler.fit_transform(np.array(X2))
+X1 = torch.tensor(np.array(X1), dtype = torch.float).to(device)
+X2 = torch.tensor(np.array(X2), dtype = torch.float).to(device)
+
+label = pd.read_table(config.label)
+time, event = torch.tensor(label['time'], dtype=torch.float), torch.tensor(label['event'], dtype=torch.float)
+time, event = time.unsqueeze(1).to(device), event.unsqueeze(1).to(device)
+train_idx, test_idx, _, _ = train_test_split(np.arange(X1.shape[0]), event, test_size = 0.2, random_state = 25)
+train_idx, val_idx, _, _ = train_test_split(train_idx, event[train_idx], test_size = 0.25, random_state = 1)
+
+train_X1 = X1[train_idx, :]
+val_X1 = X1[val_idx, :]
+test_X1 = X1[test_idx, :]
+train_X2 = X2[train_idx, :]
+val_X2 = X2[val_idx, :]
+test_X2 = X2[test_idx, :]
+
+train_time = time[train_idx]
+val_time = time[val_idx]
+test_time = time[test_idx]
+train_event = event[train_idx]
+val_event = event[val_idx]
+test_event = event[test_idx]
+
+training_data = Data(train_X1, train_X2, train_time, train_event, device)
+val_data = Data(val_X1, val_X2, val_time, val_event, device)
+test_data = Data(test_X1, test_X2, test_time, test_event, device)
+
+train_dataloader = DataLoader(training_data, batch_size = train_idx.size)
+val_dataloader = DataLoader(val_data, batch_size = val_idx.size)
+test_dataloader = DataLoader(test_data, batch_size = test_idx.size)
+
 encoder1 = Encoder(input_dim, 256)
 decoder1 = Decoder(64, 256, input_dim)
 encoder2 = Encoder(input_dim, 256)
@@ -16,67 +74,221 @@ decoder2 = Decoder(64, 256, input_dim)
 model = MLSurv(encoder1, decoder1, encoder2, decoder2)
 model = model.to(device)
 
-learning_rate = 5e-5
-epochs = 500
+learning_rate = 1e-6
+n_epochs = 1500
+batch_size = 256
+patience = 1500
 
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-criterion1 = nn.MSELoss()   # NEED FIX : to VAE loss
-criterion2 = nn.CrossEntropyLoss()  # NEED FIX : to SurvLoss
+mse_loss = nn.MSELoss()
+surv_loss = SurvLoss(device)
 
-# loss_axis = []
-# cindex_train = []
-# cindex_test = []
-
-def train_loop(dataloader, model, loss_fn1, loss_fn2, optimizer, t):
-    size = len(dataloader.dataset)
-    for batch, (X, time, observed) in enumerate(dataloader):
-        # Compute prediction and loss
-        pred = model(X)
-        pred_score = model(X)   # NEED FIX : for calculating c-index
-        loss = loss_fn1(pred) + loss_fn2(pred)  # NEED FIX
-
-        # Backpropagation
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        
-        train_perf = 0
-        train_perf = concordance_index(event_times = time.cpu().detach().numpy(),
-                                       event_observed = observed.cpu().detach().numpy(),
-                                       predicted_scores = -pred_score.cpu().detach().numpy())
-        
-        if ((t+1) % 25 == 0):
-            print(f"Epoch {t+1}\n-------------------------------")
-            print(f"Train Accuracy: {train_perf}\n")
-            print(f"Train Loss: {loss.item()}\n")
-        # print(f"Type of loss: {type(loss.item())}") # output: float
-        
-        # loss_axis.append(loss.item())
-        # cindex_train.append(train_perf)
-
-# iterate over test dataset to check model performance
-def test_loop(dataloader, model, t):
-    size = len(dataloader.dataset)
-    test_perf = 0
-
-    with torch.no_grad():
-        for X, time, observed in dataloader:
-            pred = model.forward(X)
-            pred_score = model.forward(X)   # NEED FIX : for calculating c-index
-            # print(f"event_times shape: {time.numpy().shape}")
-            # print(f"predicted shape: {pred.numpy().shape}\n")
-            test_perf = concordance_index(event_times = time.cpu().detach().numpy(),
-                                          event_observed = observed.cpu().detach().numpy(),
-                                          predicted_scores = -pred_score.cpu().detach().numpy())
-
+def train_model(model, patience, n_epochs, optimizer):
     
-    if ((t+1) % 50 == 0):
-        print(f"Test Accuracy: {test_perf}\n")
+    # Track training loss
+    train_losses = []
+    # Track validation loss
+    valid_losses = []
+    # Track average training loss
+    avg_train_losses = []
+    # Track average validation loss
+    avg_valid_losses = []
+    # Track average c-index for training data
+    avg_train_acc = []
+    # Track average c-index for validation data
+    avg_valid_acc = []
+    
+    t_vae_losses = []
+    t_disentangle_losses = []
+    t_surv_losses = []
+    v_vae_losses = []
+    v_disentangle_losses = []
+    v_surv_losses = []
+    
+    
+    # early_stopping object
+    early_stopping = EarlyStopping(patience = patience, verbose = True)
+    
+    for epoch in range(1, n_epochs + 1):
+        model.train()
+        for batch, (X1, X2, time_batch, event_batch) in enumerate(train_dataloader, 1):
+            # loss = 0
+            # Compute predition and loss
+            decoder1_, decoder2_, decoder3_, decoder4_, comm1, spe1, comm2, spe2, mu1, sigma1, mu2, sigma2, output, inputmlp = model.forward(X1, X2)
+            # VAE Loss
+            vae_loss = mse_loss(decoder1_, X1) + mse_loss(decoder2_, X2) + mse_loss(decoder3_, X1) + mse_loss(decoder4_, X2) + \
+                latent_loss(mu1, sigma1) + latent_loss(mu2, sigma2)
+            # Disentangle Loss
+            loss_inher = mse_loss(comm1, comm2)
+            loss_var = mse_loss(spe1, spe2)
+            disentangle_loss = loss_inher - loss_var
+            # Survival Prediction Loss
+            surv_error = surv_loss.forward(risk = output, times = time_batch, events = event_batch, breaks = model.output_intervals.double().to(device))
+            # Total loss
+            loss = 2*vae_loss + \
+                disentangle_loss + \
+                200*surv_error
+                
+            # Backpropagation
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
         
-    # cindex_test.append(test_perf)
+            # Check train performance
+            train_losses.append(loss.item())
+            t_vae_losses.append(vae_loss.item())
+            t_disentangle_losses.append(disentangle_loss.item())
+            t_surv_losses.append(surv_error.item())
+            
+            
+        surv = pd.DataFrame(output.detach().cpu().numpy()).T
+        durations = torch.flatten(train_time).detach().cpu().numpy()
+        events = torch.flatten(train_event).detach().cpu().numpy()
+        ev = EvalSurv(surv, durations, events, censor_surv='km')
+        train_ctd = ev.concordance_td('antolini')
+    
+        # validate   
+        model.eval()
+        for X1, X2, time_batch, event_batch in val_dataloader:
+            decoder1_, decoder2_, decoder3_, decoder4_, comm1, spe1, comm2, spe2, mu1, sigma1, mu2, sigma2, output, inputmlp = model.forward(X1, X2)
+            # VAE Loss
+            vae_loss = mse_loss(decoder1_, X1) + mse_loss(decoder2_, X2) + mse_loss(decoder3_, X1) + mse_loss(decoder4_, X2) + \
+                latent_loss(mu1, sigma1) + latent_loss(mu2, sigma2)
+            # Disentangle Loss
+            loss_inher = mse_loss(comm1, comm2)
+            loss_var = mse_loss(spe1, spe2)
+            disentangle_loss = loss_inher - loss_var
+            # Survival Prediction Loss
+            surv_error = surv_loss.forward(risk = output, times = time_batch, events = event_batch, breaks = model.output_intervals.double().to(device))
+            # Total Loss
+            loss = 2*vae_loss + \
+                disentangle_loss + \
+                200*surv_error
+            valid_losses.append(loss.item())
+            v_vae_losses.append(vae_loss.item())
+            v_disentangle_losses.append(disentangle_loss.item())
+            v_surv_losses.append(surv_error.item())
+        surv = pd.DataFrame(output.detach().cpu().numpy()).T
+        durations = torch.flatten(val_time).detach().cpu().numpy()
+        events = torch.flatten(val_event).detach().cpu().numpy()
+        ev = EvalSurv(surv, durations, events, censor_surv='km')
+        valid_ctd = ev.concordance_td('antolini')
 
-for t in range(epochs):
-    model.train()
-    train_loop(train_dataloader, model, criterion1, criterion2, optimizer, t)
+        train_loss = np.average(train_losses)
+        valid_loss = np.average(valid_losses)
+        avg_train_losses.append(train_loss)
+        avg_valid_losses.append(valid_loss)
+        avg_train_acc.append(train_ctd)
+        avg_valid_acc.append(valid_ctd)
+        
+        t_vae_loss = np.average(t_vae_losses)
+        t_disentangle_loss = np.average(t_disentangle_losses)
+        t_surv_loss = np.average(t_surv_losses)
+        v_vae_loss = np.average(v_vae_losses)
+        v_disentangle_loss = np.average(v_disentangle_losses)
+        v_surv_loss = np.average(v_surv_losses)
+
+        
+        epoch_len = len(str(n_epochs))
+        # print_msg = (f'[{epoch:>{epoch_len}}/{n_epochs:>{epoch_len}}] ' +
+        #              f'train_loss: {train_loss:.5f} ' +
+        #              f'valid_loss: {valid_loss:.5f}')
+        # print(print_msg)
+        #if (epoch % 100 == 0):
+        print_msg = (f'[{epoch:>{epoch_len}}/{n_epochs:>{epoch_len}}] \n' +
+                f'TRAIN\n' + 
+                f'vae : {t_vae_loss:.5f} | disentangle : {t_disentangle_loss:.5f} | surv : {t_surv_loss:.5f} | total loss : {train_loss:.5f}\n' +
+                f'VALIDATION\n' +
+                f'vae : {v_vae_loss:.5f} | disentangle : {v_disentangle_loss:.5f} | surv : {v_surv_loss:.5f} | total loss : {valid_loss:.5f}')
+        print(print_msg)
+
+        # clear lists to track next epoch
+        train_losses = []
+        valid_losses = []
+
+        # make check point when validation loss decreases
+        # early_stopping(valid_loss, model)
+
+        if early_stopping.early_stop:
+            print("Early stopping")
+            break
+
+   # best model이 저장되어있는 last checkpoint를 로드한다.
+    model.load_state_dict(torch.load('checkpoint1.pt'))
+    
+    return  model, avg_train_losses, avg_valid_losses, avg_train_acc, avg_valid_acc
+    
+# iterate over test dataset to check model performance
+def test_loop(dataloader, model):
+    print("---------------- Start test -------------------")
     model.eval()
-    test_loop(test_dataloader, model, t)
+    with torch.no_grad():
+        for X1, X2, time_batch, event_batch in dataloader:
+            decoder1_, decoder2_, decoder3_, decoder4_, comm1, spe1, comm2, spe2, mu1, sigma1, mu2, sigma2, output, inputmlp = model.forward(X1, X2)
+    
+        surv = pd.DataFrame(output.detach().cpu().numpy()).T
+        durations = torch.flatten(test_time).detach().cpu().numpy()
+        events = torch.flatten(test_event).detach().cpu().numpy()
+        ev = EvalSurv(surv, durations, events, censor_surv='km')
+        test_ctd = ev.concordance_td('antolini')
+        # VAE Loss
+        vae_loss = mse_loss(decoder1_, X1) + mse_loss(decoder2_, X2) + mse_loss(decoder3_, X1) + mse_loss(decoder4_, X2) + \
+                latent_loss(mu1, sigma1) + latent_loss(mu2, sigma2)
+        # Disentangle Loss
+        loss_inher = mse_loss(comm1, comm2)
+        loss_var = mse_loss(spe1, spe2)
+        disentangle_loss = loss_inher - loss_var
+        # Survival Prediction Loss
+        surv_error = surv_loss.forward(risk = output, times = time_batch, events = event_batch, breaks = model.output_intervals.double().to(device))
+        # Total Loss
+        loss = 2*vae_loss + \
+            disentangle_loss + \
+            200*surv_error
+        print(f"Test Accuracy : ")
+        print(f"VAE loss: {vae_loss:>7f},\
+            Disentangle loss: {disentangle_loss:>7f},\
+            Surv loss: {surv_error:>7f},\
+            Total loss: {loss:>7f},\
+            Ctd : {test_ctd}")
+        
+# Visualize loss     
+def visualize_loss(t_loss, v_loss):
+    fig = plt.figure(figsize=(10,8))
+    plt.plot(range(1,len(t_loss)+1), t_loss, label='Training Loss')
+    plt.plot(range(1,len(v_loss)+1), v_loss, label='Validation Loss')
+
+    # validation loss의 최저값 지점을 찾기
+    minposs = v_loss.index(min(v_loss))+1
+    plt.axvline(minposs, linestyle='--', color='r', label='Early Stopping Checkpoint')
+
+    plt.xlabel('epochs')
+    plt.ylabel('loss')
+    plt.ylim(-100, 100)
+    plt.xlim(0, len(t_loss)+1)
+    plt.grid(True)
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+    fig.savefig('MLSurv_loss_plot1.png', bbox_inches = 'tight')
+    
+# Visualize c-td     
+def visualize_cindex(t_acc, v_acc):
+    fig = plt.figure(figsize=(10,8))
+    plt.plot(range(1,len(t_acc)+1), t_acc, label='Training C-td')
+    plt.plot(range(1,len(v_acc)+1), v_acc, label='Validation C-td')
+
+    plt.xlabel('epochs')
+    plt.ylabel('c-td')
+    plt.ylim(0.4, 1.0)
+    plt.xlim(0, len(t_acc)+1)
+    plt.grid(True)
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+    fig.savefig('MLSurv_c-td_plot1.png', bbox_inches = 'tight')
+    
+
+model, train_loss, valid_loss, train_acc, valid_acc = train_model(model, patience, n_epochs, optimizer) 
+visualize_loss(train_loss, valid_loss)
+visualize_cindex(train_acc, valid_acc)
+test_loop(test_dataloader, model)
