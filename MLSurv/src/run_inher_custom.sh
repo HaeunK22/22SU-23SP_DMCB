85f86cfbca9b95edd2f99b85bff7da7d9d62dc68
HaeunK22 <69032913+HaeunK22@users.noreply.github.com>
2023-03-15 20:10:20 +0900

    For running concurrently

---
+++ MLSurv/src/run_inher_custom.sh
@@ -0,0 +1,8 @@
+python train_inher_custom.py -c 1,1,10 -gpu 1&
+python train_inher_custom.py -c 1,1,100 -gpu 1&
+python train_inher_custom.py -c 2,1,10 -gpu 1&
+python train_inher_custom.py -c 2,1,100 -gpu 1&
+python train_inher_custom.py -c 2,1,200 -gpu 1&
+python train_inher_custom.py -c 5,1,10 -gpu 1&
+python train_inher_custom.py -c 10,1,10 -gpu 1&
+python train_inher_custom.py -c 10,1,20 -gpu 1&
