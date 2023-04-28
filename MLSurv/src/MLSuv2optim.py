1f1349d063da36064bad0bc1bb098a448b5c110c
HaeunK22 <69032913+HaeunK22@users.noreply.github.com>
2023-04-28 17:16:49 +0900

    ERROR  buffers have already been freed.
    RuntimeError: Trying to backward through the graph a second time, but the buffers have already been freed. Specify retain_graph=True when calling backward the first time.

---
+++ MLSurv/src/MLSuv2optim.py
@@ -0,0 +1,98 @@
+"""MLSurv for two optimizers."""
+import torch
+from torch.autograd import Variable
+import numpy as np
+import torch.nn.functional as F
+
+class Encoder(torch.nn.Module):
+    def __init__(self, D_in, H):
+        super(Encoder, self).__init__()
+        self.linear1 = torch.nn.Linear(D_in, H)
+
+    def forward(self, x):
+        return F.relu(self.linear1(x))
+
+
+class Decoder(torch.nn.Module):
+    def __init__(self, latent, H, D_out):
+        super(Decoder, self).__init__()
+        self.linear1 = torch.nn.Linear(latent, H)
+        self.linear2 = torch.nn.Linear(H, D_out)
+        
+    def forward(self, x):
+        x = F.relu(self.linear1(x))
+        return F.relu(self.linear2(x))
+
+
+class VAE(torch.nn.Module):
+    def __init__(self, input_dim, dim1, dim2):
+        super(VAE, self).__init__()
+        self.encoder1 = Encoder(input_dim, dim1)
+        self.decoder1 = Decoder(dim2, dim1, input_dim)
+        self.encoder2 = Encoder(input_dim, dim1)
+        self.decoder2 = Decoder(dim2, dim1, input_dim)
+
+        self.hidden_layer1 = dim1
+        self.hidden_layer2 = dim2
+        
+        self._enc_mu = torch.nn.Linear(dim1, dim2)
+        self._enc_log_sigma = torch.nn.Linear(dim1, dim2)
+        
+    def _sample_latent(self, h_enc):
+        """
+        Return the latent normal sample z ~ N(mu, sigma^2)
+        """
+        mu = self._enc_mu(h_enc)
+        log_sigma = self._enc_log_sigma(h_enc)
+        sigma = torch.exp(log_sigma)
+        std_z = torch.from_numpy(np.random.normal(0, 1, size=sigma.size())).float().to(mu.device)
+
+        self.z_mean = mu
+        self.z_sigma = sigma
+
+        return mu + sigma * Variable(std_z, requires_grad=False), mu, sigma  # Reparameterization trick
+    
+    def forward(self, x1, x2):
+        # VAE 1
+        h_enc1 = self.encoder1(x1)  # Linear : input_dim -> hidden_dim1(default:500)
+        z1, mu1, sigma1 = self._sample_latent(h_enc1)   # Latent vector, size hidden_dim2(default:200)
+
+        comm1, spe1 = z1.split([int(self.hidden_layer2/2), int(self.hidden_layer2/2)], dim=1)
+        decoder1_ = self.decoder1(z1)   # Decoder1 output : hidden_dim2(default:200) -> input_dim
+
+        # VAE 2
+        h_enc2 = self.encoder2(x2)
+        z2, mu2, sigma2 = self._sample_latent(h_enc2)
+        
+        comm2, spe2 = z2.split([int(self.hidden_layer2/2), int(self.hidden_layer2/2)], dim=1)
+        decoder2_ = self.decoder2(z2)
+
+        # variational from latent 1 & inherent from latent 2
+        connect1 = torch.cat([comm2, spe1], dim=1)
+        decoder3_ = self.decoder1(connect1) # Decoder3 output
+        
+        # variational from latent 2 & inherent from latent 1
+        connect2 = torch.cat([comm1, spe2], dim=1)
+        decoder4_ = self.decoder2(connect2) # Decoder4 output
+
+        # input of final risk layer
+        inputmlp_com = (comm1 + comm2) / 2
+        inputmlp = torch.cat((inputmlp_com, spe1, spe2), 1)
+        
+        return decoder1_, decoder2_, decoder3_, decoder4_, comm1, spe1, comm2, spe2, mu1, sigma1, mu2, sigma2, inputmlp
+    
+    
+class MLSurv(torch.nn.Module):
+    def __init__(self, input_dim, dim1, dim2):
+        super(MLSurv, self).__init__()
+        self.vae = VAE(input_dim, dim1, dim2)
+        self.risk_layer = torch.nn.Sequential(
+            torch.nn.Linear(int(dim2/2*3), 30, bias=True),
+            torch.nn.Sigmoid())
+        self.output_intervals=torch.arange(0., 365 * 31, 365)
+
+    def forward(self, x1, x2):
+        decoder1_, decoder2_, decoder3_, decoder4_, comm1, spe1, comm2, spe2, mu1, sigma1, mu2, sigma2, inputmlp = self.vae(x1, x2)
+        out = self.risk_layer(inputmlp)
+
+        return decoder1_, decoder2_, decoder3_, decoder4_, comm1, spe1, comm2, spe2, mu1, sigma1, mu2, sigma2, out, inputmlp
