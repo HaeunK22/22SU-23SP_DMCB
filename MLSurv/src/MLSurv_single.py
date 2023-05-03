095a78f13436465104f1f301cd7ff2b816cf50e9
HaeunK22 <69032913+HaeunK22@users.noreply.github.com>
2023-05-03 17:12:37 +0900

    Add comments

--- MLSurv/src/MLSurv_single.py
+++ MLSurv/src/MLSurv_single.py
@@ -3,9 +3,10 @@ from torch.autograd import Variable
 import numpy as np
 import torch.nn.functional as F
 
-class Encoder1(torch.nn.Module):
+
+class Encoder(torch.nn.Module):
     def __init__(self, D_in, H):
-        super(Encoder1, self).__init__()
+        super(Encoder, self).__init__()
         self.linear1 = torch.nn.Linear(D_in, H)
 
     def forward(self, x):
@@ -13,30 +14,31 @@ class Encoder1(torch.nn.Module):
         return F.relu(self.linear1(x))
 
 
-class Decoder1(torch.nn.Module):
+class Decoder(torch.nn.Module):
     def __init__(self, latent, H, D_out):
-        super(Decoder1, self).__init__()
+        super(Decoder, self).__init__()
         self.linear1 = torch.nn.Linear(latent, H)
         self.linear2 = torch.nn.Linear(H, D_out)
+        
     def forward(self, x):
         x = F.relu(self.linear1(x))
         return F.relu(self.linear2(x))
 
 
 class MLSurv_single(torch.nn.Module):
-    latent_dim = 64
 
-    def __init__(self, encoder, decoder):
+    def __init__(self, input_dim, dim1, dim2):
         super(MLSurv_single, self).__init__()
-        self.encoder = encoder
-        self.decoder = decoder
-        self._enc_mu = torch.nn.Linear(256, 64)
-        self._enc_log_sigma = torch.nn.Linear(256, 64)
+        self.encoder = Encoder(input_dim, dim1)
+        self.decoder = Decoder(dim2, dim1, input_dim)
+        
+        self._enc_mu = torch.nn.Linear(dim1, dim2)
+        self._enc_log_sigma = torch.nn.Linear(dim1, dim2)
         self.output_intervals=torch.arange(0., 365 * 31, 365)
         
 
         self.risk_layer = torch.nn.Sequential(
-            torch.nn.Linear(64, 30, bias=True),
+            torch.nn.Linear(int(dim2), 30, bias=True),
             torch.nn.Sigmoid())
 
     def _sample_latent(self, h_enc):
@@ -55,14 +57,12 @@ class MLSurv_single(torch.nn.Module):
         return mu + sigma * Variable(std_z, requires_grad=False), mu, sigma  # Reparameterization trick
 
     def forward(self, x):
-        h_enc1 = self.encoder(x)
-        # print("h_enc1", h_enc1)
-        z1, mu1, sigma1 = self._sample_latent(h_enc1)
-        # print("z1", z1)
-        # print("mu1", mu1)
-        # print("sigma1", sigma1)
-        decoder_ = self.decoder(z1)
-        # print("decoder_", decoder_)
-        out = self.risk_layer(z1)
+        h_enc1 = self.encoder(x)    # Encoder( Linear 3000 -> 500 / relu )
 
-        return decoder_,mu1,sigma1,out,z1
+        z1, mu1, sigma1 = self._sample_latent(h_enc1)   # Gaussian Distribution: ( Linear 500 -> 200 )
+
+        decoder_ = self.decoder(z1)
+
+        out = self.risk_layer(z1)   # Risk Layer ( Linear 200 -> 30 / Sigmoid)
+
+        return decoder_, mu1, sigma1, out, z1
